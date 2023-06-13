defaac7af697550e3e9a503a1fef97910b2692a0
Tyler Nickerson <nickersoft@gmail.com>
2023-06-13 14:43:46 -0700

    landing

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    fontFamily: {
+      display: ['Playfair Display', 'serif'],
+      sans: ['Inter', 'sans-serif'],
+    },
+    colors: {
+      black: '#1B1D21',
+      white: '#FFFFFF',
+      red: '#FF5757',
+      yellow: '#FFD954',
+      green: '#72FF66'
+    }
+	},
+	plugins: [],
+} satisfies Config;
