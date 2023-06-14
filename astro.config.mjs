ccf50e0cbfddfb04dbf7839b7a716733954725c3
Tyler Nickerson <nickersoft@gmail.com>
2023-06-13 19:23:28 -0700

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import { join } from 'node:path';
+import { defineConfig } from "astro/config";
+import { join } from "node:path";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), prefetch()],
 });
