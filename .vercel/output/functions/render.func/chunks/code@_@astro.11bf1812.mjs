549ce2cf7955de6bc495eb0ce86d036c84854d04
Tyler Nickerson <nickersoft@gmail.com>
2023-07-06 23:04:15 -0700

    wip

--- .vercel/output/functions/render.func/chunks/code@_@astro.11bf1812.mjs
+++
@@ -1,7 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import './astro.b0a1b10b.mjs';
-
-const page = () => import('./prerender.c11daa90.mjs').then(n => n.c);
-
-export { page };
