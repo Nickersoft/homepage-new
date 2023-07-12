2344aea2212caf727c5024dc73654b3bd9dc3b21
Tyler Nickerson <nickersoft@gmail.com>
2023-07-12 12:28:53 -0700

    fix(*): fix build

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import microphone from "~icons/mdi/microphone";
+import globe from "~icons/mdi/globe";
+import monkey from "~icons/game-icons/gorilla";
+import terminal from "~icons/mdi/terminal";
+import moon from "~icons/solar/moon-bold";
+import book from "~icons/el/book";
+import email from "~icons/mdi/email";
+import notification from "~icons/mingcute/notification-fill";
+
 export const SITE_TITLE = "Tyler Nickerson";
 
 export const SITE_DESCRIPTION = "The personal homepage of Tyler Nickerson";
@@ -15,16 +24,13 @@ export const TAGS: Record<string, string> = {
   Tailwind: "https://tailwindcss.com",
 };
 
-export const ICONS: Record<
-  string,
-  { default: astroHTML.JSX.Element }
-> = {
-  microphone: await import("~icons/mdi/microphone"),
-  globe: await import("~icons/mdi/globe"),
-  monkey: await import("~icons/game-icons/gorilla"),
-  terminal: await import("~icons/mdi/terminal"),
-  moon: await import("~icons/solar/moon-bold"),
-  book: await import("~icons/el/book"),
-  email: await import("~icons/mdi/email"),
-  notification: await import("~icons/mingcute/notification-fill"),
+export const ICONS: Record<string, astroHTML.JSX.Element> = {
+  microphone,
+  globe,
+  monkey,
+  terminal,
+  moon,
+  book,
+  email,
+  notification,
 };
