9675df15059857753007392d68e5b1eb6ff61e78
Tyler Nickerson <nickersoft@gmail.com>
2025-05-27 22:04:25 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,32 @@
 import { defineConfig } from "astro/config";
 import { join } from "node:path";
 
+import tailwind from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import Icons from "unplugin-icons/vite";
 
 import rehypeExternalLinks from "rehype-external-links";
-import rehypeFigure from '@microflash/rehype-figure';
-import remarkGFM from 'remark-gfm';
+import rehypeFigure from "@microflash/rehype-figure";
+import remarkGFM from "remark-gfm";
 
 const rehypePlugins = [rehypeExternalLinks, rehypeFigure];
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tylernickerson.com",
-  output: "hybrid",
+  output: "static",
   integrations: [mdx({ optimize: true }), sitemap(), tailwind()],
   adapter: vercel(),
   prefetch: true,
   markdown: {
     remarkPlugins: [remarkGFM],
-    rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }], rehypeFigure] 
+    rehypePlugins: [[rehypeExternalLinks, { target: "_blank" }], rehypeFigure],
   },
   vite: {
     plugins: [
+      tailwind(),
       Icons({
         compiler: "astro",
       }),
